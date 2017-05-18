c775490446305590014154048a0b96cd4fe60a13
erasmolm <erostheworst@gmail.com>
2017-05-18 16:58:50 +0200

    Add files via upload

---
+++ ER_Interrupt_BARE/Debug/src/stm32f4xx_it.d
@@ -0,0 +1,193 @@
+src/stm32f4xx_it.o: ../src/stm32f4xx_it.c \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_conf.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device/stm32f4xx.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device/stm32f407xx.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cm4.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cmInstr.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/cmsis_gcc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cmFunc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cmSimd.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device/system_stm32f4xx.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_can.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_crc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_cryp.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dac.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_eth.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_sram.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_nor.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_nand.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pccard.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_hash.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rng.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_sd.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_usart.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_irda.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ F:/Erasmo/Documents/UNI/SE/workspaceSE/ER_Interrupt_BARE/inc/stm32f4xx_it.h
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_conf.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device/stm32f4xx.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device/stm32f407xx.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cm4.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cmInstr.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/cmsis_gcc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cmFunc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core/core_cmSimd.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device/system_stm32f4xx.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_can.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_crc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_cryp.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dac.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_eth.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_sram.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_nor.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_nand.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pccard.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_hash.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rng.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_sd.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_usart.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_irda.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+F:/Erasmo/Documents/UNI/SE/workspaceSE/ER_Interrupt_BARE/inc/stm32f4xx_it.h:
---
+++ ER_Interrupt_BARE/Debug/startup/subdir.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../startup/startup_stm32f407xx.s 
+
+OBJS += \
+./startup/startup_stm32f407xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+startup/%.o: ../startup/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Assembler'
+	@echo %cd%
+	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/ER_Interrupt_BARE/inc" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/core" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/CMSIS/device" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc/Legacy" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/HAL_Driver/Inc" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ampire480272" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ampire640480" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/Common" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/cs43l22" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/exc7200" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ft6x06" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ili9325" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ili9341" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/l3gd20" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/lis302dl" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/lis3dsh" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ls016b8uy" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/lsm303dlhc" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/mfxstm32l152" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/n25q128a" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/n25q256a" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/n25q512a" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/otm8009a" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ov2640" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/s25fl512s" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/s5k5cag" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/st7735" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/st7789h2" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/stmpe1600" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/stmpe811" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/ts3510" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/Components/wm8994" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities" -I"F:/Erasmo/Documents/UNI/SE/workspaceSE/stm32f4discovery_hal_lib/Utilities/STM32F4-Discovery" -g -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
